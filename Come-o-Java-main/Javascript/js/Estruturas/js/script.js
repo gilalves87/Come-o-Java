1ff1a42168dea3c9f6fc250a0113dc6694e14906
gilalves87 <gilvan_alves25@hotmail.com>
2025-10-27 19:44:43 -0300

    Atualizado

---
+++ Come-o-Java-main/Javascript/js/Estruturas/js/script.js
@@ -0,0 +1,72 @@
+//let nome = "João";
+//console.log("gilvan");
+//console.log(nome);
+
+//nome = "João Silva";
+//console.log(nome);
+
+//const idade = 25;
+//console.log(idade);
+
+//console.log(typeof nome);
+//console.log(typeof idade);
+
+// Variaveis
+//let a, b, c;
+//a = 10;
+//b = 20;
+//c = 35;
+//console.log(a, b, c);
+
+//const nomeCompleto = "Gilvan Alves";
+//console.log(nomeCompleto);
+
+//const nomecompleto = "Ana Beatriz";
+//console.log(nomecompleto);
+
+//let $nome = "Sport";
+//let _nome = "Corinthians";
+//console.log($nome, _nome);
+// Prompt
+//const age = prompt("Digite sua idade:");
+//console.log (`você tem ${age} anos`);
+
+//alert
+//alert("Bem vindo ao site!");
+//const z = 5;
+//alert(`O valor de a é ${z}`);
+
+// Math
+//console.log(Math.max(5, 10, 25, 3, 50, 2));
+//console.log(Math.floor(4.2));
+//console.log(Math.ceil(4.3));
+
+//Console
+//console.log("Olá, Mundo!");
+//console.error("Isso é um erro!");
+//console.warn("Isso é um aviso!");
+
+//if
+//const numero = 10;
+//if (numero > 5) {
+    //console.log("O número é maior que 5.");
+//}
+
+//const user = "Gilvan";
+//if (user === "Gilvan") {
+    //console.log("Bem-vindo, Gilvan!");
+//}
+//else 
+    //const loggedIn = false;
+    //if (loggedIn) {
+        //console.log("Usuário logado.");
+    //} else {
+        //console.log("Por favor, faça login.");
+    //}
+    //const q=10
+    //const w=25
+    //if (q > 5 && w > 20) {
+        //console.log("Numeros Maiores");
+    //} else {
+        //console.log("Numeros Menores");
+    //}
